a00f182c8c787eead3d2402cee9e6fc6a74f8d7e
jegarcia <garciaj.uk@gmail.com>
2016-09-09 10:40:47 +0100

    fixing regexp

--- ipcenter.py
+++ ipcenter.py
@@ -7,7 +7,7 @@ weechat.register("ipcenter_python", "JerryGarcia", "1.0", "GPL3", "IPCenter Scri
 weechat.prnt("", "%s[IPcenter]%s Script Loaded (by Jerry Garcia)" % (weechat.color("white,red"),weechat.color("reset")))
 def modifier_cb(data, modifier, modifier_data, string):
         finals = string
-        match = re.search('([0-9]{8})',string)
+        match = re.search('([0-9]{8}(\s|$))',string)
         if match:
             ipcenter = "https://ipcenter.ipsoft.com/IPim/Ticket/Display.html?id="+match.group()
             weechat.prnt(weechat.current_buffer(), "%s[IPCenter] %s%s" % (weechat.color("red") ,weechat.color("reset") , ipcenter))
